0a2491853aab9909621428ed4c86976f1daa2ac8
Jimmaliaros <jimmaliaros@gmail.com>
2025-03-29 21:24:24 +0200

    finished Product class

---
+++ Product.java
@@ -0,0 +1,45 @@
+public class Product {
+    String name;
+    String type;
+    long amount;
+    double price;
+
+    public Product(String name, String type, long amount, double price) {
+        this.name = name;
+        this.type = type;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
