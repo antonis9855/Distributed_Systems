411c27d6e65f41f2b0207f3575ef224ed954486f
Jimmaliaros <jimmaliaros@gmail.com>
2025-03-29 21:08:42 +0200

    made Store class

---
+++ CosnoleAppManager.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class CosnoleApp {
+    public static  void main(String[] main){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type the name of the store: ");
+
+    }
+
+}
