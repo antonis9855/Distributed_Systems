7aaab3dec733a57131e1a674cf5fa0749547eee2
super <super@SUPERCOM_I5-WIN>
2025-03-29 14:26:39 +0200

    0.2

--- Master.java
+++ Master.java
@@ -3,22 +3,33 @@ import java.net.*;
 
 public class Master {
     private static final int PORT = 5000;
+    private static final String[] WORKER_IPS = {"192.168.1.101", "192.168.1.102", "192.168.1.103"};
+    private static final int WORKER_PORT = 6000;
+    private static int currentWorker = 0;
+
+  
+    public static synchronized String getNextWorkerIP() {
+        String workerIP = WORKER_IPS[currentWorker];
+        currentWorker = (currentWorker + 1) % WORKER_IPS.length;
+        return workerIP;
+    }
+
+    public static int getWorkerPort() {
+        return WORKER_PORT;
+    }
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName("0.0.0.0"))) {
             System.out.println("Master started on port " + PORT);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                // Create a new thread for each client request
                 new WorkerThread(clientSocket).start();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
-
-
